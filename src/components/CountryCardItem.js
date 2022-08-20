635f28807cf2e94f44f78a0ff3b712b9d41bc8fc
ajhughesdev <andrew@ajhughes.dev>
2022-08-19 20:45:54 -0400

    html validation

--- src/components/CountryCardItem.js
+++ src/components/CountryCardItem.js
@@ -1,41 +1,40 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 
-import { AppContext } from '../App.provider'
+import { AppContext } from "../App.provider";
 
 const CountryCardItem = (props) => {
-  const { theme } = useContext(AppContext)
+  const { theme } = useContext(AppContext);
 
   return (
     <article
-      className={theme === 'light' ? 'light' : ''}
+      className={theme === "light" ? "light" : ""}
       onClick={props.onClick}
     >
       <img
-        className='flagImg'
-        loading='lazy'
-        height='160'
-        width='264'
+        className="flagImg"
+        height="160"
+        width="264"
         src={props.flag}
         alt={`${props.name}'s flag`}
       />
-      <div className='info'>
+      <div className="info">
         <h2 title={props.name}>{props.name}</h2>
         <ul>
           <li>
-            <span className='info-item'>Population: </span>
+            <span className="info-item">Population: </span>
             {props.population.toLocaleString()}
           </li>
           <li>
-            <span className='info-item'>Region: </span>
+            <span className="info-item">Region: </span>
             {props.region}
           </li>
           <li>
-            <span className='info-item'>Capital: </span>
-            {props.capital || 'None'}
+            <span className="info-item">Capital: </span>
+            {props.capital || "None"}
           </li>
         </ul>
       </div>
     </article>
-  )
-}
-export default CountryCardItem
+  );
+};
+export default CountryCardItem;
